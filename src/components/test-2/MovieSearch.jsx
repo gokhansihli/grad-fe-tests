4a4a937b652d404052beb1d6994b686a88d13734
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-01 21:53:23 +0100

    Implemented movies table

--- src/components/test-2/MovieSearch.jsx
+++ src/components/test-2/MovieSearch.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import movieData from "../../movies.json";
+
 export default function MovieSearch() {
-  return <div>MovieSearch</div>;
+  const [input, setInput] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by title, Director, Genre or Actor/Actress"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setInput(e.target.value);
+          }}
+        />
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Year</th>
+            <th>Genre</th>
+            <th>Cast</th>
+            <th>Average Rating</th>
+          </tr>
+          {movieData.map((movie) => {
+            return (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.director}</td>
+                <td>{movie.year}</td>
+                <td>{movie.genre}</td>
+                <td>{movie.cast}</td>
+                <td>{movie.ratings.iMDB}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
