b6de76f4e5cb844b9269dde78f96f59ee70db11f
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-01 21:16:32 +0100

    Added test-2 route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import LoginPage from "./components/test-1/LoginPage";
 import { Routes, Route } from "react-router";
+import LoginPage from "./components/test-1/LoginPage";
+import MovieSearch from "./components/test-2/MovieSearch";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/test-1" element={<LoginPage />} />
+      <Route path="/test-2" element={<MovieSearch />} />
     </Routes>
   );
 }
