7bf51e912aa693c431970275983f15ddec930903
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-02 21:37:13 +0100

    Added input searching logic

--- src/components/test-2/MovieSearch.jsx
+++ src/components/test-2/MovieSearch.jsx
@@ -9,9 +9,10 @@ export default function MovieSearch() {
       <div>
         <input
           type="text"
+          value={input}
           placeholder="Search by title, Director, Genre or Actor"
           onChange={(e) => {
-            console.log(e.target.value);
+            // console.log(e.target.value);
             setInput(e.target.value);
           }}
         />
@@ -27,16 +28,40 @@ export default function MovieSearch() {
             <th>Average Rating</th>
           </tr>
           {movieData.map((movie) => {
-            return (
-              <tr>
-                <td>{movie.title}</td>
-                <td>{movie.director}</td>
-                <td>{movie.year}</td>
-                <td>{movie.genre}</td>
-                <td>{movie.cast}</td>
-                <td>{movie.ratings.iMDB}</td>
-              </tr>
-            );
+            const titleInput = movie.title
+              .toLowerCase()
+              .includes(input.toLowerCase());
+
+            const directorInput = movie.director
+              .toLowerCase()
+              .includes(input.toLowerCase());
+
+            const genreInput = movie.genre
+              .toLowerCase()
+              .includes(input.toLowerCase());
+
+            const castInput = movie.cast.some((actor) => {
+              actor.toLowerCase().includes(input.toLowerCase());
+            });
+
+            const ratings = Object.values(movie.ratings).flat();
+            ratings[0] *= 10;
+            const averageRating = (
+              ratings.reduce((sum, val) => sum + val, 0) / ratings.length
+            ).toFixed(2);
+
+            if (titleInput || directorInput || genreInput || castInput) {
+              return (
+                <tr>
+                  <td>{movie.title}</td>
+                  <td>{movie.director}</td>
+                  <td>{movie.year}</td>
+                  <td>{movie.genre}</td>
+                  <td>{movie.cast.join(", ")}</td>
+                  <td>{averageRating}</td>
+                </tr>
+              );
+            }
           })}
         </tbody>
       </table>
