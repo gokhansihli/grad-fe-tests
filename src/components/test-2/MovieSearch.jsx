22027e311684c88dc9c3c97d70441699283495fe
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-03 20:24:45 +0100

    Changed the layout

--- src/components/test-2/MovieSearch.jsx
+++ src/components/test-2/MovieSearch.jsx
@@ -6,75 +6,78 @@ export default function MovieSearch() {
   const [input, setInput] = useState("");
 
   return (
-    <div>
-      <div>
+    <div className="main-content">
+      <h1>Movie List Filter</h1>
+      <div className="movie-search">
         <input
+          className="input"
           type="text"
           value={input}
-          placeholder="Search movie"
+          placeholder="Search by Title, Director, Genre, Year or Cast"
           onChange={(e) => {
             setInput(e.target.value);
           }}
         />
       </div>
-      <table>
-        <tbody>
-          <tr>
-            <th>Title</th>
-            <th>Director</th>
-            <th>Year</th>
-            <th>Genre</th>
-            <th>Cast</th>
-            <th>Average Rating</th>
-          </tr>
-          {movieData.map((movie) => {
-            const titleInput = movie.title
-              .toLowerCase()
-              .includes(input.toLowerCase());
 
-            const directorInput = movie.director
-              .toLowerCase()
-              .includes(input.toLowerCase());
+      <div className="movie-content">
+        {movieData.map((movie) => {
+          const titleInput = movie.title
+            .toLowerCase()
+            .includes(input.toLowerCase());
 
-            const genreInput = movie.genre
-              .toLowerCase()
-              .includes(input.toLowerCase());
+          const directorInput = movie.director
+            .toLowerCase()
+            .includes(input.toLowerCase());
 
-            const yearInput = movie.year
-              .toString()
-              .includes(input.toLowerCase());
+          const genreInput = movie.genre
+            .toLowerCase()
+            .includes(input.toLowerCase());
 
-            const castInput = movie.cast.some((actor) => {
-              actor.toLowerCase().includes(input.toLowerCase());
-            });
+          const yearInput = movie.year.toString().includes(input.toLowerCase());
 
-            const ratings = Object.values(movie.ratings).flat();
-            ratings[0] *= 10;
-            const averageRating = (
-              ratings.reduce((sum, val) => sum + val, 0) / ratings.length
-            ).toFixed(2);
+          const castInput = movie.cast.some((actor) =>
+            actor.toLowerCase().includes(input.toLowerCase()),
+          );
 
-            if (
-              titleInput ||
-              directorInput ||
-              genreInput ||
-              yearInput ||
-              castInput
-            ) {
-              return (
-                <tr key={movie.title}>
-                  <td>{movie.title}</td>
-                  <td>{movie.director}</td>
-                  <td>{movie.year}</td>
-                  <td>{movie.genre}</td>
-                  <td>{movie.cast.join(", ")}</td>
-                  <td>{averageRating}</td>
-                </tr>
-              );
-            }
-          })}
-        </tbody>
-      </table>
+          const ratings = Object.values(movie.ratings).flat();
+          ratings[0] *= 10;
+          const averageRating = (
+            ratings.reduce((sum, val) => sum + val, 0) / ratings.length
+          ).toFixed(2);
+
+          if (
+            titleInput ||
+            directorInput ||
+            genreInput ||
+            yearInput ||
+            castInput
+          ) {
+            return (
+              <div className="movie-card" key={movie.title}>
+                <img
+                  src={movie.image}
+                  alt={movie.title}
+                  className="movie-img"
+                />
+                <div className="movie-info">
+                  <h4>{movie.title}</h4>
+                  <p>
+                    {movie.year} • {movie.genre}
+                  </p>
+                  <p>&#9733; {averageRating}</p>
+                  <p>
+                    <span>Director:</span> {movie.director}
+                  </p>
+                  <p>
+                    <span>Cast:</span> {movie.cast.join(", ")}
+                  </p>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
