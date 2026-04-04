648bcb27db2f85736acd131ba161b7fa934723a3
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-04 20:15:57 +0100

    Added RegionGuess Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 import LoginPage from "./components/test-1/LoginPage";
 import MovieSearch from "./components/test-2/MovieSearch";
+import RegionGuess from "./components/test-3/RegionGuess";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/test-1" element={<LoginPage />} />
       <Route path="/test-2" element={<MovieSearch />} />
+      <Route path="/test-3" element={<RegionGuess />} />
     </Routes>
   );
 }
