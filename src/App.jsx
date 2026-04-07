ac45bc3e2c828f2509a51c4306d78748303af55c
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-07 21:03:00 +0100

    Added component route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import LoginPage from "./components/test-1/LoginPage";
 import MovieSearch from "./components/test-2/MovieSearch";
 import RegionGuess from "./components/test-3/RegionGuess";
+import FindX from "./components/test-4/FindX";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/test-1" element={<LoginPage />} />
       <Route path="/test-2" element={<MovieSearch />} />
       <Route path="/test-3" element={<RegionGuess />} />
+      <Route path="/test-4" element={<FindX />} />
     </Routes>
   );
 }
