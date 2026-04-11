6ff0e33438afcb3b14bd9a8ddef3601971c3c5bb
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-11 19:14:05 +0100

    Added useEffect and table

--- src/components/test-4/FindX.jsx
+++ src/components/test-4/FindX.jsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./findX.css";
 
 export default function FindX() {
-  const [count, setCount] = useState(0);
+  const [newGame, setNewGame] = useState([]);
+  const [found, setFound] = useState(0);
+  const [score, setScore] = useState("");
+
+  let xCoor = Math.floor(Math.random() * 18);
+  let yCoor = Math.floor(Math.random() * 7);
+
+  useEffect(() => {
+    let game = new Array(7);
+    game.fill([]);
+
+    game = game.map((set) => {
+      const arr = new Array(18);
+      arr.fill(null);
+      return arr;
+    });
+    game[yCoor][xCoor] = "x";
+    setNewGame(game);
+
+    if (found === 1) setScore("You have found X 1 time!");
+    else setScore(`You have found X ${found} times!`);
+  }, [found]);
 
   return (
     <div>
-      <h3>You have found X {count} times!</h3>
-      <table className="table-container"></table>
+      <h2>You have found X {found} times!</h2>
+      <table className="table-container">
+        <tbody>
+          {newGame.map((row, rowIndex) => {
+            return (
+              <tr key={rowIndex}>
+                {row.map((tile, tileIndex) => {
+                  if (tile === "x") {
+                    return (
+                      <td key={tileIndex}>
+                        <button
+                          className="x"
+                          onClick={() => setFound((num) => num + 1)}
+                        >
+                          X
+                        </button>
+                      </td>
+                    );
+                  }
+                  return <td key={tileIndex}>{tile}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
