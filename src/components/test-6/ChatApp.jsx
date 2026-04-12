bd5dae363435d93b379fab92b43de74008de90e3
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-12 20:46:20 +0100

    Changed form order

--- src/components/test-6/ChatApp.jsx
+++ src/components/test-6/ChatApp.jsx
@@ -11,41 +11,6 @@ export default function ChatApp() {
   return (
     <div className="chat-container">
       <form
-        id="chat1Form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          let chat = chat1.trim();
-          if (chat.length > 0) {
-            setGenChat((genChat) => {
-              const newGenChat = [...genChat];
-              newGenChat.push(user1 + ":" + chat);
-              return newGenChat;
-            });
-            setChat1("");
-          }
-        }}
-      >
-        <input
-          type="text"
-          value={user1}
-          onChange={(e) => setUser1(e.target.value)}
-        />
-        <br />
-        <br />
-        <textarea
-          id="chat1"
-          value={chat1}
-          onChange={(e) => setChat1(e.target.value)}
-          cols="30"
-          rows="20"
-        ></textarea>
-        <br />
-        <button className="send" htmlFor="chat1Form">
-          Send
-        </button>
-      </form>
-      <br />
-      <form
         onSubmit={(e) => {
           e.preventDefault();
           setGenChat([]);
@@ -58,7 +23,7 @@ export default function ChatApp() {
           id="genChat"
           value={genChat.join("\n")}
           readOnly
-          cols="70"
+          cols="40"
           rows="20"
         ></textarea>
         <br />
@@ -67,40 +32,79 @@ export default function ChatApp() {
         </button>
       </form>
       <br />
-      <form
-        id="chat2Form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          let chat = chat2.trim();
-          if (chat.length > 0) {
-            setGenChat((genChat) => {
-              const newGenChat = [...genChat];
-              newGenChat.push(user2 + ":" + chat);
-              return newGenChat;
-            });
-            setChat2("");
-          }
-        }}
-      >
-        <input
-          type="text"
-          value={user2}
-          onChange={(e) => setUser2(e.target.value)}
-        />
+      <div className="user-chat">
+        <form
+          id="chat1Form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            let chat = chat1.trim();
+            if (chat.length > 0) {
+              setGenChat((genChat) => {
+                const newGenChat = [...genChat];
+                newGenChat.push(user1 + ":" + chat);
+                return newGenChat;
+              });
+              setChat1("");
+            }
+          }}
+        >
+          <input
+            className="chat-input"
+            type="text"
+            value={user1}
+            onChange={(e) => setUser1(e.target.value)}
+          />
+          <br />
+          <br />
+          <textarea
+            id="chat1"
+            value={chat1}
+            onChange={(e) => setChat1(e.target.value)}
+            cols="30"
+            rows="20"
+          ></textarea>
+          <br />
+          <button className="send" htmlFor="chat1Form">
+            Send
+          </button>
+        </form>
         <br />
-        <br />
-        <textarea
-          id="chat2"
-          value={chat2}
-          onChange={(e) => setChat2(e.target.value)}
-          cols="30"
-          rows="20"
-        ></textarea>
-        <br />
-        <button className="send" htmlFor="chat2Form">
-          Send
-        </button>
-      </form>
+        <form
+          id="chat2Form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            let chat = chat2.trim();
+            if (chat.length > 0) {
+              setGenChat((genChat) => {
+                const newGenChat = [...genChat];
+                newGenChat.push(user2 + ":" + chat);
+                return newGenChat;
+              });
+              setChat2("");
+            }
+          }}
+        >
+          <input
+            className="chat-input"
+            type="text"
+            value={user2}
+            onChange={(e) => setUser2(e.target.value)}
+          />
+          <br />
+          <br />
+          <textarea
+            id="chat2"
+            value={chat2}
+            onChange={(e) => setChat2(e.target.value)}
+            cols="30"
+            rows="20"
+          ></textarea>
+          <br />
+          <button className="send" htmlFor="chat2Form">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
