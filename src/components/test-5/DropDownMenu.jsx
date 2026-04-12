a0755ad9231668cda0ea7920bf939b34965db8fb
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-12 12:13:01 +0100

    Added menu  html structure

---
+++ src/components/test-5/DropDownMenu.jsx
@@ -0,0 +1,61 @@
+export default function DropDownMenu() {
+  return (
+    <div>
+      <ul id="menu">
+        <li className="menu">
+          <a href="#">Home</a>
+        </li>
+        <li className="menu">
+          <a href="#">About Us</a>
+        </li>
+        <li className="menu">
+          <a href="#">Tests</a>
+          <div className="dropDown">
+            <ul>
+              <li>
+                <a href="#">Test 1</a>
+              </li>
+              <li>
+                <a href="#">Test 2</a>
+              </li>
+              <li>
+                <a href="#">Test 3</a>
+              </li>
+              <li>
+                <a href="#">Test 4</a>
+              </li>
+              <li>
+                <a href="#">Test 5</a>
+              </li>
+              <li>
+                <a href="#">Test 6</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="menu">
+          <a href="#">Careers</a>
+          <div className="dropDown">
+            <ul>
+              <li>
+                <a href="https://northcoders.com/">Nortcoders</a>
+              </li>
+              <li>
+                <a href="https://www.apple.com/">Apple</a>
+              </li>
+              <li>
+                <a href="https://www.google.com">Google</a>
+              </li>
+              <li>
+                <a href="https://www.twitter.com">Twitter</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className="menu">
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
